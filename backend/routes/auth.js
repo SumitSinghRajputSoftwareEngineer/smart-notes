bbebb745437149c329db105f7072d79a7d58cff4
Sumit Singh Rajput <sumitsinghrajput786gwalior@gmail.com>
2022-10-28 22:21:22 +0530

    create user and login authentication part completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
-const router = express.Router();
-const User = require("../models/User");
-
+const { body, validationResult } = require("express-validator"); //for validation
+const router = express.Router(); //routing
+const User = require("../models/User"); //User schema
+const bcrypt = require("bcryptjs"); //to implement hashing and salting for password security
+const jwt = require("jsonwebtoken");
 //create a user using : POST "/api/auth/createuser".. Doesn't require login
+
+const JWT_SECRET = "SumitSinghRajput";
+
 router.post(
   "/createuser",
   [
@@ -20,21 +24,89 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     //check whether the user with this email exist already
-    let user = await User.findOne({email: req.body.email});
-    if(user){
-        return res.status(400).json({error: "Sorry a user with this email already exists"})
-    }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      
-    // .then((user) => res.json(user))
-    //   .catch((err) => {console.log(err)
-    // res.json({error: 'Please enter a unique value for email', message: err.message})});
-    res.json({"userCreated":' yes user has been created'})
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      const salt = await bcrypt.genSalt(10); //generating salt
+      const secPass = await bcrypt.hash(req.body.password, salt); //adding hashing and salt
+      //create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
 
+      // .then((user) => res.json(user))
+      //   .catch((err) => {console.log(err)
+      // res.json({error: 'Please enter a unique value for email', message: err.message})});
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      console.log(authToken);
+      //   res.json(user);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//Verify the login of a user using : POST "/api/auth/login"..  Doesn't require signup
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    let success = false;
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        success = false;
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        success = false;
+        return res
+          .status(400)
+          .json({
+            success,
+            error: "Please try to login with correct credentials",
+          });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      success = true;
+      res.json({ success, authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
